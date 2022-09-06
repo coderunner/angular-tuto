d9e3eee1e6709a45d09e00b7820db6a98823ccc7
felixtrepanier <felixtrepanier@gmail.com>
2022-09-06 16:42:12 -0400

    ajout de la variation avec subscription

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Person } from '../model/person';
 import { PersonService } from '../services/person.service';
 
@@ -9,22 +9,35 @@ import { PersonService } from '../services/person.service';
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css'],
 })
-export class PersonsComponent implements OnInit {
-  // Angular nous injecte les dépendances!
-  constructor(
-    private personService: PersonService,
-    private formBuilder: FormBuilder
-  ) {}
-
-  persons$: Observable<Person[]> = this.personService.getPersons();
-
+export class PersonsComponent implements OnInit, OnDestroy {
   // On crée la structure du formulaire.
   // On assigne le formulaire et ses champs dans le HTML
   form = this.formBuilder.group({
     name: '',
   });
 
+  persons$: Observable<Person[]> = this.personService.getPersons();
+
+  persons: Person[] = [];
+  personSubscription: Subscription | undefined = undefined;
+
+  // Angular nous injecte les dépendances!
+  constructor(
+    private personService: PersonService,
+    private formBuilder: FormBuilder
+  ) {
+    this.personSubscription = this.personService
+      .getPersons()
+      .subscribe((persons) => (this.persons = persons));
+  }
+
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    if (this.personSubscription) {
+      this.personSubscription.unsubscribe();
+    }
+  }
 
   onSubmit(): void {
     if (this.form.value.name) {
